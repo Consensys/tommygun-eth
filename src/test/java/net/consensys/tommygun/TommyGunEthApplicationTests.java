91239a5d758725da6e8d1a8fc009253ae5465a21
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-11-16 17:59:37 +0100

    fix unit tests

--- src/test/java/net/consensys/tommygun/TommyGunEthApplicationTests.java
+++ src/test/java/net/consensys/tommygun/TommyGunEthApplicationTests.java
@@ -8,8 +8,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
-    args =
-        "--account-creator-private-key=0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63")
+    args = {
+      "--account-creator-private-key=0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
+      "--state-storage-creator-private-key=0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f"
+    })
 class TommyGunEthApplicationTests {
 
   @Test
@@ -17,8 +19,11 @@ class TommyGunEthApplicationTests {
 
   @Test
   void applicationArgumentsPopulated(@Autowired ApplicationArguments args) {
-    assertThat(args.getOptionNames()).containsOnly("account-creator-private-key");
+    assertThat(args.getOptionNames())
+        .contains("account-creator-private-key", "state-storage-creator-private-key");
     assertThat(args.getOptionValues("account-creator-private-key"))
         .containsOnly("0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63");
+    assertThat(args.getOptionValues("state-storage-creator-private-key"))
+        .containsOnly("0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f");
   }
 }
